7941048105f0aa5bfda83f9f6dceedae019cf654
saeidha <saeed221373@yahoo.com>
2025-08-21 19:31:21 +0330

    change on src/LockToken.sol Solidity contract Protocol

--- src/LockToken.sol
+++ src/LockToken.sol
@@ -124,3 +124,6 @@ emit TokensWithdrawn(msg.sender, _lockId, amount);
     /**
      * @notice Returns the total number of locks created.
      */
+        function getLockCount() external view returns (uint256) {
+        return locks.length;
+    }

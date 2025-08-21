fe5af7b2ceeaa62b48fbd3cdb1cc16c495808332
saeidha <saeed221373@yahoo.com>
2025-08-21 19:03:40 +0330

    import locked token

--- src/LockToken.sol
+++ src/LockToken.sol
@@ -16,3 +16,16 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 contract LockToken is Ownable, Pausable {
     using SafeERC20 for IERC20;
 
+   // --- Events ---
+    event TokensLocked(address indexed user, uint256 lockId, uint256 amount, uint256 unlockTime);
+    event TokensWithdrawn(address indexed user, uint256 lockId, uint256 amount);
+    event LockExtended(uint256 indexed lockId, uint256 newUnlockTime);
+    event EmergencyWithdrawal(address indexed token, address indexed to, uint256 amount);
+
+  // --- Structs ---
+    struct Lock {
+        address owner;
+        uint256 amount;
+        uint256 unlockTime;
+        bool active; // To mark if the lock has been withdrawn
+    }

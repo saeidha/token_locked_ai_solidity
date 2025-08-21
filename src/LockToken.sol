270e5b063b02ce85ac20d78ac907fc44e4f919b5
saeidha <saeed221373@yahoo.com>
2025-08-21 19:02:23 +0330

    import

--- src/LockToken.sol
+++ src/LockToken.sol
@@ -5,3 +5,10 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @title LockToken
+ * @author Gemini
+ * @notice A contract for time-locking a specific ERC20 token.
+ * Users can lock tokens for a chosen duration and withdraw them only after the lock expires.
+ */

100c17c7619dcef6c55b232da06f384263d55cd1
saeidha <saeed221373@yahoo.com>
2025-08-24 18:14:36 +0330

    change on sample2/Loanly.sol Solidity contract Module

--- sample2/Loanly.sol
+++ sample2/Loanly.sol
@@ -76,7 +76,7 @@ contract Loanly {
         emit LoanFunded(_id, msg.sender, loan.amount);
         }
 
-/**
+    /**
      * @dev Repays a funded loan.
      * @param _id The ID of the loan to repay.
      */

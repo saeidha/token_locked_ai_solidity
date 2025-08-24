19229cbc72925bec77f2b4dd03b65e1c70834ecc
saeidha <saeed221373@yahoo.com>
2025-08-24 18:40:43 +0330

    change on sample2/Loanly.t.sol Solidity contract Enhancement

--- sample2/Loanly.t.sol
+++ sample2/Loanly.t.sol
@@ -122,3 +122,5 @@ contract LoanlyTest is Test {
     function testCalculateInterest() public {
         vm.prank(borrower);
         loanly.requestLoan(LOAN_AMOUNT, INTEREST_RATE, DURATION);
+vm.prank(lender);
+        loanly.fundLoan{value: LOAN_AMOUNT}(1);

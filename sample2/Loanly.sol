063a05f89da7b1af62513c5a75dc0cab604ca041
saeidha <saeed221373@yahoo.com>
2025-08-24 15:40:15 +0330

    change on sample2/Loanly.sol Solidity contract Handling

--- sample2/Loanly.sol
+++ sample2/Loanly.sol
@@ -5,3 +5,5 @@ pragma solidity ^0.8.20;
  * @title Loanly
  * @dev A simple smart contract for peer-to-peer lending with interest.
  */
+contract Loanly {
+    // A structure to hold the details of each loan.

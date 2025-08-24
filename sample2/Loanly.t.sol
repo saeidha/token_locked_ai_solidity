e2916b58de4b9c8875eaaee1f4a896085ae4b9e9
saeidha <saeed221373@yahoo.com>
2025-08-24 18:32:43 +0330

    change on sample2/Loanly.t.sol Solidity contract Adjustment

--- sample2/Loanly.t.sol
+++ sample2/Loanly.t.sol
@@ -41,4 +41,8 @@ contract LoanlyTest is Test {
      */
     function testFundLoan() public {
         vm.prank(borrower);
-        loanly.requestLo
+        vm.prank(borrower);
+        loanly.requestLoan(LOAN_AMOUNT, INTEREST_RATE, DURATION);
+
+        vm.prank(lender);
+        loanly.fundLoan{value: LOAN_AMOUNT}(1);

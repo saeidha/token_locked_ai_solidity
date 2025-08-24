6e6c44c9de9ba2ae703000235e4627e98146f6c8
saeidha <saeed221373@yahoo.com>
2025-08-24 15:40:20 +0330

    change on sample2/Loanly.sol Solidity contract Refactor

--- sample2/Loanly.sol
+++ sample2/Loanly.sol
@@ -7,3 +7,14 @@ pragma solidity ^0.8.20;
  */
 contract Loanly {
     // A structure to hold the details of each loan.
+    struct Loan {
+        uint256 id;
+        address payable borrower;
+        address payable lender;
+        uint256 amount;
+        uint256 interest; // Basis points, e.g., 500 for 5%
+        uint256 duration; // in seconds
+        uint256 startTime;
+        bool funded;
+        bool repaid;
+    }

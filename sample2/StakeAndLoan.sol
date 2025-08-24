300e4e1cc7756a955e0edd6ead5354fc1f8f8142
saeidha <saeed221373@yahoo.com>
2025-08-25 02:21:27 +0330

    change on sample2/StakeAndLoan.sol Solidity contract Cleanup

--- sample2/StakeAndLoan.sol
+++ sample2/StakeAndLoan.sol
@@ -196,5 +196,6 @@ contract StakeAndLoan is Ownable {
      */
 
         function getAccountMaxBorrowableValue(address _user) public view returns (uint256) {
-
+            uint256 collateralValue = getCollateralValue(stakedBalance[_user]);
+            return (collateralValue * collateralizationRatio) / 10000;
 }

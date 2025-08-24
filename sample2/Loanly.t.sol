b6dc35cefea794bdfff07d54dffb63cee4fdcdf8
saeidha <saeed221373@yahoo.com>
2025-08-24 18:28:14 +0330

    fix

--- sample2/Loanly.t.sol
+++ sample2/Loanly.t.sol
@@ -5,3 +5,15 @@ import "forge-std/Test.sol";
 import "../src/Loanly.sol";
 contract LoanlyTest is Test {
     Loanly public loanly;
+    address public borrower = address(1);
+    address public lender = address(2);
+    uint256 public constant LOAN_AMOUNT = 1 ether;
+    uint256 public constant INTEREST_RATE = 500; // 5%
+    uint256 public constant DURATION = 30 days;
+
+    /**
+     * @dev Sets up the testing environment before each test.
+     */
+    function setUp() public {
+        loanly = new Loanly();
+        

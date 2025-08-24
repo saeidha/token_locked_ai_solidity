81a8af52d5fc27797aab7c2e0c5527cc71592fca
saeidha <saeed221373@yahoo.com>
2025-08-25 01:57:53 +0330

    change on sample2/StakeAndLoan.sol Solidity contract State

--- sample2/StakeAndLoan.sol
+++ sample2/StakeAndLoan.sol
@@ -3,3 +3,9 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title StakeAndLoan
+ * @dev A contract that allows users to stake collateral tokens and borrow loan tokens.
+ */
+contract StakeAndLoan is Ownable {

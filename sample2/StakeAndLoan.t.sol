fa8cc606019ce41605327621b0619313b86ffb90
saeidha <saeed221373@yahoo.com>
2025-08-26 00:09:06 +0330

    add MockERC20

--- sample2/StakeAndLoan.t.sol
+++ sample2/StakeAndLoan.t.sol
@@ -4,3 +4,8 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import "../src/StakeAndLoan.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock Token", "MTK") {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }

4a3c2c14ed8a85cfe81e87ce7f41d47579fab4a3
saeidha <saeed221373@yahoo.com>
2025-08-26 00:24:55 +0330

    change on sample2/StakeAndLoan.t.sol Solidity contract Protocol

--- sample2/StakeAndLoan.t.sol
+++ sample2/StakeAndLoan.t.sol
@@ -182,3 +182,4 @@ contract StakeAndLoanTest is Test {
      */
         function testGetCollateralValue() public {
                     uint256 value = stakeAndLoan.getCollateralValue(1 ether);
+                            // 1 ether * 2000 / 1e18 = 2000

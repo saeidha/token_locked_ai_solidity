f3e9f65d12e9398c4b6c769a8ba32573df13d93b
saeidha <saeed221373@yahoo.com>
2025-08-27 23:49:17 +0330

    change on sample2/YieldFarm.sol Solidity contract State

--- sample2/YieldFarm.sol
+++ sample2/YieldFarm.sol
@@ -4,3 +4,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+/**
+ * @title YieldFarm
+ * @dev A contract for staking tokens to earn rewards with optional time-locks for higher APY.
+ */
+contract YieldFarm is Ownable, ReentrancyGuard {

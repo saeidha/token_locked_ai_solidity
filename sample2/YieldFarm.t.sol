f4acddf111af1ef7e6363b7f25f980229f1270b0
saeidha <saeed221373@yahoo.com>
2025-08-28 18:18:33 +0330

    change on sample2/YieldFarm.t.sol Solidity contract State

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -36,3 +36,4 @@ contract YieldFarmTest is Test {
         // Approve the yield farm to spend user's staking tokens
         vm.prank(user1);
         stakingToken.approve(address(yieldFarm), type(uint256).max);
+    }

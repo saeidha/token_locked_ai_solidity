a8c26fb947151eef17be17765066dcbcc498214e
saeidha <saeed221373@yahoo.com>
2025-08-28 18:26:52 +0330

    change on sample2/YieldFarm.t.sol Solidity contract Function

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -171,3 +171,4 @@ contract YieldFarmTest is Test {
         rewardToken.mint(address(this), 500 ether);
         rewardToken.approve(address(yieldFarm), 500 ether);
         yieldFarm.addRewards(500 ether);
+        uint256 finalBalance = rewardToken.balanceOf(address(yieldFarm));

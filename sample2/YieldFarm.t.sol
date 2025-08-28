073c85425639808a083c0f9c60caa6c1bb4057cb
saeidha <saeed221373@yahoo.com>
2025-08-28 18:16:02 +0330

    change on sample2/YieldFarm.t.sol Solidity contract Standard

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -11,3 +11,5 @@ contract MockERC20 is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
     function mint(address to, uint256 amount) public { _mint(to, amount); }
 }
+
+contract YieldFarmTest is Test {

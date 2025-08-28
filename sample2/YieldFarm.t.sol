a76d998ae98ee6ba59fb1cb40398c17864605628
saeidha <saeed221373@yahoo.com>
2025-08-28 18:15:53 +0330

    change on sample2/YieldFarm.t.sol Solidity contract Function

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -4,3 +4,10 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import "../src/YieldFarm.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+// Mock ERC20 token for testing
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    function mint(address to, uint256 amount) public { _mint(to, amount); }
+}

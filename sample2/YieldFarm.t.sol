eec4544bfeacbdedb8923db33169b665a1b75bac
saeidha <saeed221373@yahoo.com>
2025-08-28 18:25:13 +0330

    change on sample2/YieldFarm.t.sol Solidity contract Handling

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -151,3 +151,4 @@ contract YieldFarmTest is Test {
     function testUnstakeAfterLockup() public {
         vm.startPrank(user1);
         stakingToken.approve(address(yieldFarm), 100 ether);
+        yieldFarm.stake(100 ether, YieldFarm.LockupTier.ThirtyDays);

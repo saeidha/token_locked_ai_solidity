f099b8dce66ef1c14f5190bcde8a83a4164cdbdb
saeidha <saeed221373@yahoo.com>
2025-08-28 18:25:18 +0330

    change on sample2/YieldFarm.t.sol Solidity contract Feature

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -152,3 +152,4 @@ contract YieldFarmTest is Test {
         vm.startPrank(user1);
         stakingToken.approve(address(yieldFarm), 100 ether);
         yieldFarm.stake(100 ether, YieldFarm.LockupTier.ThirtyDays);    
+        

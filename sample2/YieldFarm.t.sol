dc7ca556d432321956bb9897a1a047f14a3ab8d2
saeidha <saeed221373@yahoo.com>
2025-08-28 18:26:09 +0330

    change on sample2/YieldFarm.t.sol Solidity contract Cleanup

--- sample2/YieldFarm.t.sol
+++ sample2/YieldFarm.t.sol
@@ -158,7 +158,7 @@ contract YieldFarmTest is Test {
      * @dev Tests that non-owners cannot set reward rates.
      */
     function testNonOwnerCannotSetRewardRate() public {        vm.startPrank(user1);
-    
+            vm.prank(user1);
         yieldFarm.setRewardRate(YieldFarm.LockupTier.NinetyDays, 1000); // 10%
         vm.stopPrank();
     }

7513607cc0472ab7085c914dbac1f04dd2a4716e
saeidha <saeed221373@yahoo.com>
2025-08-31 20:51:10 +0330

    change on sample2/Raffle.sol Solidity contract Correction

--- sample2/Raffle.sol
+++ sample2/Raffle.sol
@@ -120,3 +120,5 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     /**
+     * @notice This is the callback function that the Chainlink VRF Coordinator calls after receiving a random number.
+     * @param requestId The unique ID of the VRF request.

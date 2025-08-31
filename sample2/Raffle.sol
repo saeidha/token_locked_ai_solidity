c7a70d3159db7ecdd4bc4ba3de7100ecfcf00921
saeidha <saeed221373@yahoo.com>
2025-08-31 20:48:49 +0330

    change on sample2/Raffle.sol Solidity contract Security

--- sample2/Raffle.sol
+++ sample2/Raffle.sol
@@ -2,3 +2,4 @@
 pragma solidity ^0.8.19;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";

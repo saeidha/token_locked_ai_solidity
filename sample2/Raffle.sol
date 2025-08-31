87eab31371d1adfcca1be6cd7d985f5e7a941b89
saeidha <saeed221373@yahoo.com>
2025-08-31 20:49:35 +0330

    change on sample2/Raffle.sol Solidity contract Improvement

--- sample2/Raffle.sol
+++ sample2/Raffle.sol
@@ -36,3 +36,5 @@ contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_raffleEntranceFee;
     uint256 private immutable i_interval;
     RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    address[] private s_players;

a723aaf76dd3f1b79b1b4c2874bbc8d16201e84c
saeidha <saeed221373@yahoo.com>
2025-08-31 20:55:40 +0330

    change on sample2/Raffle.t.sol Solidity contract Access

--- sample2/Raffle.t.sol
+++ sample2/Raffle.t.sol
@@ -55,3 +55,7 @@ contract RaffleTest is Test {
                            enterRaffle
     //////////////////////////////////////////////////////////////*/
     function testRaffleRevertsWhenYouDontPayEnough() public {
+        vm.prank(PLAYER);
+        vm.expectRevert(Raffle.Raffle__SendMoreToEnterRaffle.selector);
+        raffle.enterRaffle();
+    }

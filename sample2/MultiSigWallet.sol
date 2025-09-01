78cf35ffa9988663ed3fd24864c183c087ff8a4d
saeidha <saeed221373@yahoo.com>
2025-09-01 19:35:53 +0330

    change on sample2/MultiSigWallet.sol Solidity contract Component

--- sample2/MultiSigWallet.sol
+++ sample2/MultiSigWallet.sol
@@ -230,3 +230,5 @@ contract MultiSigWallet {
         if (requiredConfirmations > owners.length) {
             changeRequiredConfirmations(owners.length);
         }
+
+        emit OwnerRemoved(_oldOwner);

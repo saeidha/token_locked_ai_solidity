ca7f861fc8a2564f75012badff1153e2952efb75
saeidha <saeed221373@yahoo.com>
2025-09-01 19:42:29 +0330

    change on sample2/MultiSigWallet.t.sol Solidity contract Improvement

--- sample2/MultiSigWallet.t.sol
+++ sample2/MultiSigWallet.t.sol
@@ -225,3 +225,5 @@ describe("MultiSigWallet", function () {
             const ownerToRemove = owner3.address;
             const removeOwnerData = multiSigWallet.interface.encodeFunctionData("removeOwner", [ownerToRemove]);
 
+            await multiSigWallet.connect(owner1).submitTransaction(await multiSigWallet.getAddress(), 0, removeOwnerData);
+            await multiSigWallet.connect(owner2).confirmTransaction(0);

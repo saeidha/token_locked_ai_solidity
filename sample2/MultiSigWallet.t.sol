203404547882db76815359e64348d0ed5a39b434
saeidha <saeed221373@yahoo.com>
2025-09-01 19:49:26 +0330

    change on sample2/MultiSigWallet.t.sol Solidity contract Fix

--- sample2/MultiSigWallet.t.sol
+++ sample2/MultiSigWallet.t.sol
@@ -146,3 +146,7 @@ contract MultiSigWalletTest is Test {
         vm.prank(owner2);
         wallet.confirmTransaction(txIndex);
         
+        // Step 3: Execute
+        uint256 initialBalance = destination.balance;
+        vm.prank(owner3); // Anyone can execute
+        vm.expectEmit(true, true, false, true);

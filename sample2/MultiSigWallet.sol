7668d372445312c69109a5c5b85b7474eb9ec207
saeidha <saeed221373@yahoo.com>
2025-09-01 19:32:15 +0330

    change on sample2/MultiSigWallet.sol Solidity contract Cleanup

--- sample2/MultiSigWallet.sol
+++ sample2/MultiSigWallet.sol
@@ -2,3 +2,6 @@
 pragma solidity ^0.8.20;
 
 /**
+ * @title MultiSigWallet
+ * @dev A wallet that requires multiple owners to confirm a transaction before execution.
+ * This contract is a foundational DeFi primitive, designed for security and gas efficiency.

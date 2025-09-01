379c050c94341a3ac961a4243f40f955a6c8bbbf
saeidha <saeed221373@yahoo.com>
2025-09-01 19:46:39 +0330

    change on sample2/MultiSigWallet.t.sol Solidity contract State

--- sample2/MultiSigWallet.t.sol
+++ sample2/MultiSigWallet.t.sol
@@ -5,3 +5,6 @@ import {Test, console} from "forge-std/Test.sol";
 import {MultiSigWallet} from "../src/MultiSigWallet.sol";
 
 contract MultiSigWalletTest is Test {
+    MultiSigWallet wallet;
+    address[] public owners;
+    address public owner1 = address(0x1);

8bb91ac857822906074babb270cad45ea60198c2
saeidha <saeed221373@yahoo.com>
2025-09-01 19:34:50 +0330

    change on sample2/MultiSigWallet.sol Solidity contract Module

--- sample2/MultiSigWallet.sol
+++ sample2/MultiSigWallet.sol
@@ -158,3 +158,5 @@ contract MultiSigWallet {
     }
 
     /**
+     * @dev Allows an owner to revoke their confirmation for a pending transaction.
+     * @param _txIndex The index of the transaction to revoke confirmation for.

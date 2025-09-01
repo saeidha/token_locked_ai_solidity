ba53e52e7c412ccabd1942651e3be0912f44765c
saeidha <saeed221373@yahoo.com>
2025-09-01 19:47:48 +0330

    change on sample2/MultiSigWallet.t.sol Solidity contract Control

--- sample2/MultiSigWallet.t.sol
+++ sample2/MultiSigWallet.t.sol
@@ -64,3 +64,4 @@ contract MultiSigWalletTest is Test {
     function test_OwnerCanSubmitTransaction() public {
         vm.prank(owner1);
         uint256 txIndex = wallet.submitTransaction(destination, 1 ether, "");
+        assertEq(txIndex, 0);

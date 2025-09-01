e3eeb9a59a28fd226467d91f6d4139ac0b7a0485
saeidha <saeed221373@yahoo.com>
2025-09-01 19:38:20 +0330

    change on sample2/MultiSigWallet.t.sol Solidity contract Optimization

--- sample2/MultiSigWallet.t.sol
+++ sample2/MultiSigWallet.t.sol
@@ -10,3 +10,5 @@ describe("MultiSigWallet", function () {
     // Hook to run before each test
     beforeEach(async function () {
         // Get signers
+        [owner1, owner2, owner3, nonOwner] = await ethers.getSigners();
+        owners = [owner1.address, owner2.address, owner3.address];

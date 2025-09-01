d8bfc0474decdf1c9512e0b103ccb0238dbbbb59
saeidha <saeed221373@yahoo.com>
2025-09-01 19:48:52 +0330

    change on sample2/MultiSigWallet.t.sol Solidity contract Event

--- sample2/MultiSigWallet.t.sol
+++ sample2/MultiSigWallet.t.sol
@@ -119,3 +119,4 @@ contract MultiSigWalletTest is Test {
         vm.expectEmit(true, true, false, true); // Don't check data
         emit ConfirmationRevoked(txIndex, owner1);
         wallet.revokeConfirmation(txIndex);
+        

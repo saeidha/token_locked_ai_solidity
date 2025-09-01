2e61c311eec5d3fbccf73bedf33f9b23b0b183d6
saeidha <saeed221373@yahoo.com>
2025-09-01 19:47:51 +0330

    change on sample2/MultiSigWallet.t.sol Solidity contract Function

--- sample2/MultiSigWallet.t.sol
+++ sample2/MultiSigWallet.t.sol
@@ -66,3 +66,4 @@ contract MultiSigWalletTest is Test {
         uint256 txIndex = wallet.submitTransaction(destination, 1 ether, "");
         assertEq(txIndex, 0);
 
+        (address dest, uint256 val, , bool executed) = wallet.getTransaction(txIndex);

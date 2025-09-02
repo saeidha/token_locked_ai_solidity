85ce9a2c18ff051b01943708bc604c9c9fea9dbc
saeidha <saeed221373@yahoo.com>
2025-09-02 17:38:54 +0330

    change on sample2/TokenVesting.sol Solidity contract Protocol

--- sample2/TokenVesting.sol
+++ sample2/TokenVesting.sol
@@ -158,3 +158,6 @@ contract TokenVesting is Ownable, ReentrancyGuard {
      * @notice Gets the duration of the vesting schedule for a beneficiary.
      */
     function getDuration(address _beneficiary) public view returns (uint64) {
+        return vestingSchedules[_beneficiary].duration;
+    }
+

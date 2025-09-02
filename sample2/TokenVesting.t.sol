8e80c5b6887743924ab9d94ec60998713fa21e4e
saeidha <saeed221373@yahoo.com>
2025-09-02 17:43:31 +0330

    change on sample2/TokenVesting.t.sol Solidity contract Fix

--- sample2/TokenVesting.t.sol
+++ sample2/TokenVesting.t.sol
@@ -4,3 +4,6 @@ pragma solidity ^0.8.20;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Test} from "forge-std/Test.sol";
+import {TokenVesting} from "../contracts/TokenVesting.sol";
+
+// Mock ERC20 token for testing purposes

fcb9c95a2f9aeabd9a1b0a1b959767fe5dfed45c
saeidha <saeed221373@yahoo.com>
2025-09-02 17:45:21 +0330

    change on sample2/TokenVesting.t.sol Solidity contract Optimization

--- sample2/TokenVesting.t.sol
+++ sample2/TokenVesting.t.sol
@@ -91,3 +91,5 @@ contract TestTokenVesting is Test {
     }
     
     function test_06_ReleaseTokens_BeforeCliff() public {
+        vm.prank(owner);
+        tokenVesting.createVestingSchedule(beneficiary1, VESTING_AMOUNT_1, startTime, DURATION, CLIFF);

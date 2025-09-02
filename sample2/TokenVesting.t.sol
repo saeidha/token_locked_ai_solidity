70613883d5fd08b3c4c9153492ff24c037ddfd52
saeidha <saeed221373@yahoo.com>
2025-09-02 17:45:58 +0330

    change on sample2/TokenVesting.t.sol Solidity contract Revision

--- sample2/TokenVesting.t.sol
+++ sample2/TokenVesting.t.sol
@@ -123,3 +123,5 @@ contract TestTokenVesting is Test {
     function test_08_ReleaseTokens_AfterVestingPeriod() public {
         vm.prank(owner);
         tokenVesting.createVestingSchedule(beneficiary1, VESTING_AMOUNT_1, startTime, DURATION, CLIFF);
+        
+        vm.warp(startTime + DURATION + 1 days);

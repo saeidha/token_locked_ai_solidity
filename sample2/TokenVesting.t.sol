47aa2fd6688be38afffc440cac97c00acc704211
saeidha <saeed221373@yahoo.com>
2025-09-02 17:45:32 +0330

    change on sample2/TokenVesting.t.sol Solidity contract State

--- sample2/TokenVesting.t.sol
+++ sample2/TokenVesting.t.sol
@@ -100,3 +100,6 @@ contract TestTokenVesting is Test {
 
         vm.prank(beneficiary1);
         vm.expectRevert("TokenVesting: No tokens available for release");
+        tokenVesting.release();
+    }
+    

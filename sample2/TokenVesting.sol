d70cc00621549b4419c2378a51ae20a0d192ce03
saeidha <saeed221373@yahoo.com>
2025-09-02 17:38:31 +0330

    change on sample2/TokenVesting.sol Solidity contract Handling

--- sample2/TokenVesting.sol
+++ sample2/TokenVesting.sol
@@ -142,3 +142,4 @@ contract TokenVesting is Ownable, ReentrancyGuard {
      * @notice Gets the details of a vesting schedule for a specific beneficiary.
      * @param _beneficiary The address of the beneficiary.
      * @return The VestingSchedule struct for the given beneficiary.
+     */

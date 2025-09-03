268393f69b490e939ee20cd24642d1ad12de81a8
saeidha <saeed221373@yahoo.com>
2025-09-03 19:13:41 +0330

    change on sample2/TestENSRegistry.t.sol Solidity contract Interface

--- sample2/TestENSRegistry.t.sol
+++ sample2/TestENSRegistry.t.sol
@@ -3,3 +3,6 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/Test.sol";
 import "../contracts/ENSRegistry.sol";
+import "../contracts/PublicResolver.sol";
+
+contract TestENSRegistry is Test {

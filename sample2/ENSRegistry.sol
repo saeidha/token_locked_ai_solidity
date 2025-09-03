9b4b4462848db623deb088f58e5d037269b1abdf
saeidha <saeed221373@yahoo.com>
2025-09-03 19:02:27 +0330

    change on sample2/ENSRegistry.sol Solidity contract Standard

--- sample2/ENSRegistry.sol
+++ sample2/ENSRegistry.sol
@@ -66,3 +66,5 @@ contract ENSRegistry is Ownable, Pausable, IERC165 {
      * @dev Returns the owner of a node.
      * @param node The node to query.
      * @return The address of the owner.
+     */
+    function owner(bytes32 node) external view returns (address) {

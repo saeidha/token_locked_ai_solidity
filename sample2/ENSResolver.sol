19f69c3715acaa9ff8ec4ff40471630cc00b21a0
saeidha <saeed221373@yahoo.com>
2025-09-03 19:09:04 +0330

    change on sample2/ENSResolver.sol Solidity contract Fix

--- sample2/ENSResolver.sol
+++ sample2/ENSResolver.sol
@@ -69,3 +69,5 @@ contract PublicResolver is ERC165 {
      * @param key The key of the text record.
      * @param value The value of the text record.
      */
+    function setText(bytes32 node, string calldata key, string calldata value) external authorised(node) {
+        texts[node][key] = value;

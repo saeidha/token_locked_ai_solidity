02ac50b89481bde11fe83507779487d4329ed28d
saeidha <saeed221373@yahoo.com>
2025-09-03 19:08:25 +0330

    change on sample2/ENSResolver.sol Solidity contract Handling

--- sample2/ENSResolver.sol
+++ sample2/ENSResolver.sol
@@ -38,3 +38,4 @@ contract PublicResolver is ERC165 {
     modifier authorised(bytes32 node) {
         // This is a simplified authorization check. A real implementation would check against the ENSRegistry.
         // For this example, we will allow anyone to set records.
+        // In a real system:

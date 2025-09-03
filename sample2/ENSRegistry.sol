529d6b28ddfbcfc913801480d0c42be7188ecd9d
saeidha <saeed221373@yahoo.com>
2025-09-03 19:01:11 +0330

    change on sample2/ENSRegistry.sol Solidity contract Event

--- sample2/ENSRegistry.sol
+++ sample2/ENSRegistry.sol
@@ -2,3 +2,6 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/introspection/IERC165.sol";
+

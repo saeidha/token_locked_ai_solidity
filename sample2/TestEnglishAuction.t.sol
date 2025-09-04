34dcfb89ca3dc961cc918f8499bb26f3ac829ffc
saeidha <saeed221373@yahoo.com>
2025-09-04 21:05:07 +0330

    change on sample2/TestEnglishAuction.t.sol Solidity contract Access

--- sample2/TestEnglishAuction.t.sol
+++ sample2/TestEnglishAuction.t.sol
@@ -95,3 +95,4 @@ contract TestEnglishAuction is Test {
         vm.prank(seller);
         auction.createAuction(address(mockNft), NFT_ID, STARTING_BID, DURATION);
         vm.prank(seller);
+        auction.startAuction();

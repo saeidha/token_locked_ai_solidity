edd4b78b0d3b275f6073fd8489c3311485d3e3a6
saeidha <saeed221373@yahoo.com>
2025-09-04 21:04:50 +0330

    change on sample2/TestEnglishAuction.t.sol Solidity contract Component

--- sample2/TestEnglishAuction.t.sol
+++ sample2/TestEnglishAuction.t.sol
@@ -82,3 +82,4 @@ contract TestEnglishAuction is Test {
         auction.createAuction(address(mockNft), NFT_ID, STARTING_BID, DURATION);
         vm.prank(seller);
         auction.startAuction();
+        

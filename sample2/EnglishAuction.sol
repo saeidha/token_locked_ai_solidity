b1395b155a8e64e122c885245d8a4b8ab8d59d8d
saeidha <saeed221373@yahoo.com>
2025-09-04 21:01:35 +0330

    change on sample2/EnglishAuction.sol Solidity contract Addition

--- sample2/EnglishAuction.sol
+++ sample2/EnglishAuction.sol
@@ -229,3 +229,5 @@ contract EnglishAuction is ReentrancyGuard {
     function getNftOwner() public view returns(address) {
         if(auction.state == AuctionState.CREATED) return auction.seller;
         if(auction.state == AuctionState.STARTED) return address(this);
+        if(auction.state == AuctionState.ENDED && auction.highestBidder != address(0)) return auction.highestBidder;
+        return auction.seller; // Canceled or Ended with no bids

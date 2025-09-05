4e2845dcb8d57fb23818d0a49e28accbdda77c47
saeidha <saeed221373@yahoo.com>
2025-09-05 18:40:13 +0330

    change on sample2/SeedDAO.sol Solidity contract Protocol

--- sample2/SeedDAO.sol
+++ sample2/SeedDAO.sol
@@ -205,3 +205,4 @@ contract DAO is Ownable {
     function queue(uint proposalId) external {
         require(state(proposalId) == ProposalState.Succeeded, "DAO: Proposal has not succeeded");
         Proposal storage p = proposals[proposalId];
+        p.executionEta = block.timestamp + executionDelay;

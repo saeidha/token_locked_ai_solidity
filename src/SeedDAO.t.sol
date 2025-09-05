8d9ef88f4c5df34b95fb1d2b330ac44688dfca3a
saeidha <saeed221373@yahoo.com>
2025-09-05 18:43:48 +0330

    change on src/SeedDAO.t.sol Solidity contract Enhancement

--- src/SeedDAO.t.sol
+++ src/SeedDAO.t.sol
@@ -13,3 +13,5 @@ contract MockGovToken is Test {
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
     mapping(address => Checkpoint[]) public checkpoints;
+    struct Checkpoint { uint32 fromBlock; uint96 votes; }
+

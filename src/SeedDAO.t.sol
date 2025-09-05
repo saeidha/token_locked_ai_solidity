6c72cbc2a25d037c17136ba336254aa6dc6391c7
saeidha <saeed221373@yahoo.com>
2025-09-05 18:45:50 +0330

    change on src/SeedDAO.t.sol Solidity contract Interface

--- src/SeedDAO.t.sol
+++ src/SeedDAO.t.sol
@@ -119,3 +119,6 @@ contract TestDAO is Test {
     
     function test_05_Fail_Vote_NotActive_Ended() public {
         test_03_CastVote_Success();
+        vm.roll(block.number + VOTING_PERIOD + 1);
+        vm.prank(voterB);
+        vm.expectRevert("DAO: Voting is not active");

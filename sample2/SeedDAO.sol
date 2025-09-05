a73d1237a1c969a056408cfab38b4512e779b123
saeidha <saeed221373@yahoo.com>
2025-09-05 18:38:26 +0330

    change on sample2/SeedDAO.sol Solidity contract Improvement

--- sample2/SeedDAO.sol
+++ sample2/SeedDAO.sol
@@ -119,3 +119,5 @@ contract DAO is Ownable {
     function castVote(uint proposalId, uint8 support) external {
         _castVote(msg.sender, proposalId, support, "");
     }
+
+    function castVoteWithReason(uint proposalId, uint8 support, string calldata reason) external {

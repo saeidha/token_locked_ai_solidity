3e38c6ce3d2f0a7cfa41bcb3dbb6ad9a51def16e
saeidha <saeed221373@yahoo.com>
2025-09-05 18:43:44 +0330

    change on src/SeedDAO.t.sol Solidity contract Control

--- src/SeedDAO.t.sol
+++ src/SeedDAO.t.sol
@@ -9,3 +9,5 @@ contract MockGovToken is Test {
     string public name = "Mock Governance Token";
     string public symbol = "MGT";
     uint8 public decimals = 18;
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;

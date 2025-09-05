32d358b65c947eb26f850e0c1fa445f8a7bc17f1
saeidha <saeed221373@yahoo.com>
2025-09-05 18:50:02 +0330

    Refactor change on src/SeedDAO.t.sol Solidity contract

--- src/SeedDAO.t.sol
+++ src/SeedDAO.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import {DAO} from "../contracts/SeedDAO.sol";
+import {DAO} from "../src/SeedDAO.sol";
 
 // Mock ERC20Votes token
 contract MockGovToken is Test {

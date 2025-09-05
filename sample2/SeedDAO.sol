8ff488c0aba1467288a1f5f4fd22c59827ad34d8
saeidha <saeed221373@yahoo.com>
2025-09-05 18:37:54 +0330

    change on sample2/SeedDAO.sol Solidity contract Logic

--- sample2/SeedDAO.sol
+++ sample2/SeedDAO.sol
@@ -89,3 +89,5 @@ contract DAO is Ownable {
         require(governanceToken.getPastVotes(msg.sender, block.number - 1) >= proposalThreshold, "DAO: Proposer does not meet proposal threshold");
         require(targets.length == values.length && targets.length == calldatas.length, "DAO: Proposal arrays must have same length");
         require(targets.length > 0, "DAO: Must provide at least one action");
+
+        proposalCount++;

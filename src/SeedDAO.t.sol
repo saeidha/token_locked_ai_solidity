9cbf8e0c50a5fb8378982e4a1691c266bddf7692
saeidha <saeed221373@yahoo.com>
2025-09-05 18:50:22 +0330

    Handling change on src/SeedDAO.t.sol Solidity contract

--- src/SeedDAO.t.sol
+++ src/SeedDAO.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import {DAO} from "../SeedDAO.sol";
+import {DAO} from "./SeedDAO.sol";
 
 // Mock ERC20Votes token
 contract MockGovToken is Test {

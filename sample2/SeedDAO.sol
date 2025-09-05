a0c63d70778e5c44fee9d2b58e62c3c3c6dc965a
saeidha <saeed221373@yahoo.com>
2025-09-05 18:40:29 +0330

    change on sample2/SeedDAO.sol Solidity contract Event

--- sample2/SeedDAO.sol
+++ sample2/SeedDAO.sol
@@ -219,3 +219,4 @@ contract DAO is Ownable {
     function getProposal(uint id) external view returns(Proposal memory) { return proposals[id]; }
     function getProposalCount() external view returns (uint) { return proposalCount; }
     function hasVoted(uint proposalId, address account) external view returns (bool) { return proposals[proposalId].receipts[account].hasVoted; }
+    function getVotes(address account, uint blockNumber) external view returns (uint) { return governanceToken.getPastVotes(account, blockNumber); }

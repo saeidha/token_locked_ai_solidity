68cbe34167a3148e338e6fdc4f5e60e73e5b4340
saeidha <saeed221373@yahoo.com>
2025-09-08 01:43:52 +0330

    Verification change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -285,3 +285,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @dev A smart contract to manage user registrations and course enrollments for a decentralized learning platform.
  * Users "sign up" for the platform and then "sign" into courses by enrolling.
  */
+contract W3SchoolSign is Ownable {
+
+    // =============================================================

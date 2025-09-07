4132ae82e7e491cd93f85749ccf962ef25d0e003
saeidha <saeed221373@yahoo.com>
2025-09-08 01:36:05 +0330

    Control change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -46,3 +46,4 @@ contract W3SchoolSign is Ownable {
 
     event UserRegistered(address indexed user, string name);
     event UserProfileUpdated(address indexed user, string newName);
+    event AdminAdded(address indexed admin);

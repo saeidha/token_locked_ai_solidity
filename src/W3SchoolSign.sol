6aad1f7667753a2a0f36c94db3ac306cf866e10e
saeidha <saeed221373@yahoo.com>
2025-09-08 01:47:50 +0330

    Addition change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -482,3 +482,5 @@ contract W3SchoolSign is Ownable {
 
         enrollments[msg.sender][_courseId] = true;
         course.enrollmentCount++;
+        users[msg.sender].enrolledCourseIds.push(_courseId);
+

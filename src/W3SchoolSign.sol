8bfd1a523ff488ab877ddf6e4154a706fb26d62b
saeidha <saeed221373@yahoo.com>
2025-09-08 01:48:38 +0330

    Interface change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -523,3 +523,7 @@ contract W3SchoolSign is Ownable {
     function getTotalCourses() external view returns (uint) {
         return courseCounter;
     }
+    
+    function getCourseDetails(uint _courseId) external view courseExists(_courseId) returns (string memory, string memory, uint, bool, uint) {
+        Course memory course = courses[_courseId];
+        return (course.name, course.description, course.enrollmentFee, course.isActive, course.enrollmentCount);

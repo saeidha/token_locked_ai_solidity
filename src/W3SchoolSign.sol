b4a0f4f7ab14df1187feb661a1d3aaf81f82b92b
saeidha <saeed221373@yahoo.com>
2025-09-08 01:45:23 +0330

    Logic change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -365,3 +365,5 @@ contract W3SchoolSign is Ownable {
      * @param _admin The address to revoke admin privileges from.
      */
     function removeAdmin(address _admin) external onlyOwner {
+        admins[_admin] = false;
+        emit AdminRemoved(_admin);

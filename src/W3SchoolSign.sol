644c243fb20a382b463e03076ff4ca7fe06dd08a
saeidha <saeed221373@yahoo.com>
2025-09-08 01:47:45 +0330

    Implementation change on src/W3SchoolSign.sol Solidity contract

--- src/W3SchoolSign.sol
+++ src/W3SchoolSign.sol
@@ -479,3 +479,5 @@ contract W3SchoolSign is Ownable {
         require(course.isActive, "W3SS: Course is not active");
         require(msg.value == course.enrollmentFee, "W3SS: Incorrect enrollment fee sent");
         require(!enrollments[msg.sender][_courseId], "W3SS: Already enrolled in this course");
+
+        enrollments[msg.sender][_courseId] = true;

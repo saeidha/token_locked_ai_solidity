3756c79e36a15e8336b9cb4d26e68288ca6b9f5c
saeidha <saeed221373@yahoo.com>
2025-09-08 22:00:17 +0330

    Feature change on src/W3SchoolSignTest.t.sol Solidity contract

--- src/W3SchoolSignTest.t.sol
+++ src/W3SchoolSignTest.t.sol
@@ -54,3 +54,5 @@ contract W3SchoolSignTest is Test {
     function testFail_NonOwnerCannotAddAdmin() public {
         vm.prank(USER_1); // A non-owner tries to add an admin
         vm.expectRevert("Ownable: caller is not the owner");
+        w3s.addAdmin(USER_2);
+    }

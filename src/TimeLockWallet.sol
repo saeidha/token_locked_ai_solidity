e97b638b2a67b7ad52b17462ba0142f5465fd483
saeidha <saeed221373@yahoo.com>
2025-09-08 22:14:03 +0330

    Event change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -102,3 +102,4 @@ contract TimeLockWallet is Ownable, Pausable {
         totalLockedFunds += _amount;
         beneficiaryCount++;
         emit BeneficiaryAdded(_beneficiary, _amount, _unlockTimestamp);
+    }

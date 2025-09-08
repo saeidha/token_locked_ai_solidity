1a2947e60fb82e2c6b5a8743966e97f760d68a6a
saeidha <saeed221373@yahoo.com>
2025-09-08 22:12:55 +0330

    Adjustment change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -47,3 +47,5 @@ contract TimeLockWallet is Ownable, Pausable {
     //                            Modifiers
     // =============================================================
 
+    modifier beneficiaryExists(address _beneficiary) {
+        require(beneficiaries[_beneficiary].isActive, "TLW: Beneficiary does not exist or is inactive");

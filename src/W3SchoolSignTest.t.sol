86481a89e9547f846cffc972f7ecf60e6c838184
saeidha <saeed221373@yahoo.com>
2025-09-08 21:59:17 +0330

    Control change on src/W3SchoolSignTest.t.sol Solidity contract

--- src/W3SchoolSignTest.t.sol
+++ src/W3SchoolSignTest.t.sol
@@ -4,3 +4,5 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {W3SchoolSign} from "../src/W3SchoolSign.sol";
 
+contract W3SchoolSignTest is Test {
+    W3SchoolSign public w3s;

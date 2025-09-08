4053b2c2b2e7274782491b5e64fa88b73893f3fb
saeidha <saeed221373@yahoo.com>
2025-09-08 22:06:55 +0330

    Revision change on src/TimeLockWallet.t.sol Solidity contract

--- src/TimeLockWallet.t.sol
+++ src/TimeLockWallet.t.sol
@@ -69,3 +69,7 @@ contract W3SchoolSignTest is Test {
         vm.prank(USER_1);
         w3s.registerUser("Alice");
         
+        vm.expectRevert("W3SS: User already registered");
+        w3s.registerUser("Alice V2");
+    }
+

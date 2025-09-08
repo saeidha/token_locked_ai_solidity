224b2cfebc60865e573a473a4e8c54b05346bd56
saeidha <saeed221373@yahoo.com>
2025-09-08 22:17:31 +0330

    Event change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -279,3 +279,7 @@ contract TimeLockWallet is Ownable, Pausable {
         return beneficiaries[_beneficiary].amountLocked;
     }
 
+    /**
+     * @notice Returns the unlock timestamp for a specific beneficiary.
+     * @param _beneficiary The address of the beneficiary.
+     * @return The unlock timestamp. Returns 0 if not an active beneficiary.

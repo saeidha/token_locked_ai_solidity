19f6ccd4fc72138235c2917980bae0657723b67c
saeidha <saeed221373@yahoo.com>
2025-09-08 22:13:48 +0330

    Revision change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -91,3 +91,4 @@ contract TimeLockWallet is Ownable, Pausable {
         require(_beneficiary != address(0), "TLW: Invalid beneficiary address");
         require(_amount > 0, "TLW: Amount must be greater than zero");
         require(address(this).balance >= totalLockedFunds + _amount, "TLW: Insufficient contract balance to lock funds");
+        require(_unlockTimestamp > block.timestamp, "TLW: Unlock timestamp must be in the future");

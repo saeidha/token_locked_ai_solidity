7fe210d4c45bf01709c789724eb98084686140a4
saeidha <saeed221373@yahoo.com>
2025-09-08 22:13:11 +0330

    Optimization change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -62,3 +62,5 @@ contract TimeLockWallet is Ownable, Pausable {
     // =============================================================
 
     constructor() Ownable(msg.sender) {}
+
+    // =============================================================

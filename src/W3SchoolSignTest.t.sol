75075047666de3cfa965712f6f0f1dd0d1527346
saeidha <saeed221373@yahoo.com>
2025-09-08 22:01:29 +0330

    Revision change on src/W3SchoolSignTest.t.sol Solidity contract

--- src/W3SchoolSignTest.t.sol
+++ src/W3SchoolSignTest.t.sol
@@ -117,3 +117,5 @@ contract W3SchoolSignTest is Test {
         vm.prank(USER_1);
         vm.expectRevert("W3SS: Incorrect enrollment fee sent");
         w3s.enroll{value: wrongFee}(1);
+    }
+    

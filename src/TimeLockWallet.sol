1cb450d0f32b432b98728b4d1bd2498fb1351687
saeidha <saeed221373@yahoo.com>
2025-09-08 22:15:30 +0330

    Verification change on src/TimeLockWallet.sol Solidity contract

--- src/TimeLockWallet.sol
+++ src/TimeLockWallet.sol
@@ -173,3 +173,6 @@ contract TimeLockWallet is Ownable, Pausable {
         emit UnlockTimeExtended(_beneficiary, oldUnlockTimestamp, _newUnlockTimestamp);
     }
 
+    /**
+     * @notice Deactivates a beneficiary, making their locked funds available to the owner (if not withdrawn).
+     * @param _beneficiary The address of the beneficiary to remove.

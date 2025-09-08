0626f0e4dbad7a3ae6cb193ac9c0652ca50cca64
saeidha <saeed221373@yahoo.com>
2025-09-08 22:01:43 +0330

    Optimization change on src/W3SchoolSignTest.t.sol Solidity contract

--- src/W3SchoolSignTest.t.sol
+++ src/W3SchoolSignTest.t.sol
@@ -128,3 +128,5 @@ contract W3SchoolSignTest is Test {
         vm.prank(USER_1);
         w3s.enroll{value: COURSE_FEE}(1);
         
+        uint256 initialOwnerBalance = OWNER.balance;
+        assertEq(address(w3s).balance, COURSE_FEE);
